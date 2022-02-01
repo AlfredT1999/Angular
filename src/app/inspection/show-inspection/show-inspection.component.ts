202c5d23dd96cc5cc0c8fe34a576c9f11d89b303
Jose Alfredo Tapia Hernandez <e-jatapia@neoris.com>
2022-02-01 11:26:46 -0600

    Show-inspection component updated

--- src/app/inspection/show-inspection/show-inspection.component.ts
+++ src/app/inspection/show-inspection/show-inspection.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { InspectionApiService } from 'src/app/inspection-api.service';
 
 @Component({
   selector: 'app-show-inspection',
@@ -6,10 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./show-inspection.component.css']
 })
 export class ShowInspectionComponent implements OnInit {
+  inspectionList$!: Observable<any[]>;
+  inspectionTypesList$!: Observable<any[]>;
+  inspectionTypesList: any = [];
 
-  constructor() { }
+  // Map to display data associate with foreign keys:
+  inspectionTypesMap: Map<number, string> = new Map();
+
+  constructor(private service:InspectionApiService) { }
 
   ngOnInit(): void {
+    this.inspectionList$ = this.service.getInspectionList();
   }
 
 }
