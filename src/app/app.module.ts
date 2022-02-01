a9553f4aeb199e003d6c6684fb0ddd51651b656f
Jose Alfredo Tapia Hernandez <e-jatapia@neoris.com>
2022-02-01 10:28:55 -0600

    app.module updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { InspectionComponent } from './inspection/inspection.component';
 import { ShowInspectionComponent } from './inspection/show-inspection/show-inspection.component';
 import { AddEditInspectionComponent } from './inspection/add-edit-inspection/add-edit-inspection.component';
-
+import { InspectionApiService } from './inspection-api.service';
 @NgModule({
   declarations: [
     AppComponent,
@@ -14,9 +15,12 @@ import { AddEditInspectionComponent } from './inspection/add-edit-inspection/add
     AddEditInspectionComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [InspectionApiService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
