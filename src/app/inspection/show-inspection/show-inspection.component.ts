12f51eb9aef9baa73815327e205da8bc3970f255
Jose Alfredo Tapia Hernandez <e-jatapia@neoris.com>
2022-02-04 14:33:15 -0600

    End of the section

--- src/app/inspection/show-inspection/show-inspection.component.ts
+++ src/app/inspection/show-inspection/show-inspection.component.ts
@@ -20,5 +20,82 @@ export class ShowInspectionComponent implements OnInit {
   ngOnInit(): void {
     this.inspectionList$ = this.service.getInspectionList();
     this.inspectionTypesList$ = this.service.getInspectionTypesList();
+    this.refreshInspectionTypesMap();
+  }
+
+  // Variables:
+  modalTitle: string = '';
+  activateAddEditInspectionComponent: boolean = false;
+  inspection: any;
+
+  modalAdd()
+  {
+    this.inspection = {
+      id: 0,
+      status: null,
+      comments: null,
+      inspectionTypeId: null,
+    }
+
+    this.modalTitle = "Add Inspection";
+    this.activateAddEditInspectionComponent = true;
+  }
+
+  modalEdit(item: any)
+  {
+    this.inspection = item;
+    this.modalTitle = "Edit inspection";
+    this.activateAddEditInspectionComponent = true;
+  }
+
+  delete(item: any)
+  {
+    if(confirm(`Are you sure you want to delete inspection ${item.id}`))
+    {
+      this.service.deleteInspection(item.id).subscribe(res => {
+        var closeModalBtn = document.getElementById('add-edit-modal-close');
+
+        if(closeModalBtn)
+        {
+          closeModalBtn.click();
+        }
+
+        var showDeleteSuccess = document.getElementById('delete-success-alert');
+
+        if(showDeleteSuccess)
+        {
+          showDeleteSuccess.style.display = "block";
+        }
+
+        setTimeout(function()
+        {
+          if(showDeleteSuccess)
+          {
+            showDeleteSuccess.style.display = "none"
+          }
+        }, 4000);
+        this.inspectionList$ = this.service.getInspectionList();
+      })
+    }
+  }
+
+  modalClose()
+  {
+    this.activateAddEditInspectionComponent = false;
+    this.inspectionList$ = this.service.getInspectionList();
+  }
+
+  refreshInspectionTypesMap()
+  {
+    this.service.getInspectionTypesList().subscribe(data => 
+    {
+      this.inspectionTypesList = data;
+
+      for(let i = 0; i < data.length; i++)
+      {
+        this.inspectionTypesMap.set(this.inspectionTypesList[i].id, this.inspectionTypesList[i]
+          .inspectionName);
+      }
+    });
   }
 }
